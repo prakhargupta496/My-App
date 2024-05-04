be91093352df8b95fe264fcbf4328779f8eef173
Prakhar Gupta <prakhargupta@Prakhars-MacBook-Air.local>
2024-05-04 15:59:34 +0530

    Set the button colors

--- src/jobCard/JobCard.js
+++ src/jobCard/JobCard.js
@@ -3,11 +3,23 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Button from '@mui/material/Button';
 import './JobCard.css';
 
 
-
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: "#94FFD8",
+      contrastText: "#black"
+    },
+    danger: {
+      main: "#10439F",
+      contrastText: "#fff"
+    }
+  }
+});
 
 export default function RecipeReviewCard() {
  
@@ -27,14 +39,17 @@ export default function RecipeReviewCard() {
       <CardContent>
         "Hello World"
       </CardContent>
+      <ThemeProvider theme={theme}>
       <div className="Buttons">
         <div className="Easy_Apply_Div">
-          <Button variant="contained" fullWidth = {true} >Easy Apply</Button>
+          <Button variant="contained" fullWidth = {true} color="neutral">Easy Apply</Button>
         </div>
         <div>
-          <Button variant="contained" fullWidth = {true}>Ask for referral</Button>
+          <Button variant="contained" fullWidth = {true} color="danger">Ask for referral</Button>
         </div>
       </div>
+      </ThemeProvider>
+      
       
     </Card>
   );
