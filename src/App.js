e829a2a4961351fdfca992a112de846946329950
Prakhar Gupta <prakhargupta@Prakhars-MacBook-Air.local>
2024-05-05 10:20:15 +0530

    Changes to add the filter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,161 @@
-import Card from './jobCard';
-import Grid from '@mui/material/Grid';
+import React, { useState } from 'react'
+import { AutoSizer, Grid } from "react-virtualized";
+import Box from "@mui/material/Box";
+import TextField from '@mui/material/TextField';
+import { BsSearch } from 'react-icons/bs';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import './App.css';
+// Components
+import JobCard from "./jobCard";
+import { Fragment} from "react";
+
+
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: "#94FFD8",
+      contrastText: "#black",
+    },
+    danger: {
+      main: "#10439F",
+      contrastText: "#fff",
+    },
+    icon: {
+      main: "#F3CA52",
+    },
+  },
+});
+
+const calculateColumnWidth = (numberOfColumns, containerWidth) =>
+  containerWidth / numberOfColumns;
+
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin accumsan dui et commodo feugiat. Fusce a pretium odio, at mattis metus. Duis in ante ex. Phasellus rhoncus erat at felis aliquam vehicula. Quisque sed tristique mauris, ut ultrices libero. Curabitur mattis risus arcu, sit amet mollis tortor varius eget. Pellentesque a lacus sed velit feugiat luctus. Sed luctus orci nec leo fermentum venenatis. Proin viverra elementum eleifend. Phasellus a neque faucibus, commodo odio sit amet, interdum ligula.Proin in arcu a mi hendrerit molestie sed sed nunc. Curabitur augue urna, ultrices in auctor pharetra, feugiat non odio. Curabitur dignissim aliquet.";
+
+const companies = [
+  [
+    {
+      name: "Amazon",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+      minSal: 15,
+    },
+    {
+      name: "Flipkart",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    },
+    {
+      name: "Apple",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    },
+  ],
+  [
+    {
+      name: "Netflix",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    },
+    {
+      name: "Weekday",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    },
+    {
+      name: "Tekion",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    },
+  ],
+  [
+    {
+      name: "Microsoft",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    },
+    {
+      name: "Google",
+      title: "Software Engineer",
+      expReq: 2,
+      location: "Bangalore",
+      jobDescription: lorem,
+    }
+  ]
+];
 
 function App() {
-  const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin accumsan dui et commodo feugiat. Fusce a pretium odio, at mattis metus. Duis in ante ex. Phasellus rhoncus erat at felis aliquam vehicula. Quisque sed tristique mauris, ut ultrices libero. Curabitur mattis risus arcu, sit amet mollis tortor varius eget. Pellentesque a lacus sed velit feugiat luctus. Sed luctus orci nec leo fermentum venenatis. Proin viverra elementum eleifend. Phasellus a neque faucibus, commodo odio sit amet, interdum ligula.Proin in arcu a mi hendrerit molestie sed sed nunc. Curabitur augue urna, ultrices in auctor pharetra, feugiat non odio. Curabitur dignissim aliquet.";
-  const companies = [{name:"Amazon",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem, minSal: 15}, {name:"Flipkart",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }, 
-  {name:"Apple",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }, {name:"Netflix",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }
-  , {name:"Weekday",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }, {name:"Tekion",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }, 
-  {name:"Microsoft",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }, {name:"Google",title:"Software Engineer",expReq:2, location:"Bangalore", jobDescription:lorem }];
-  return (
-    <div className="App">
+  // const [filter, setFilter] = useState();
+  const [company, setCompanies] = useState(companies);
+  const [searchVal, setSearchVal] = useState("");
+  function handleSearchClick() {
+    if (searchVal === "") { setCompanies(companies); return; }
+    const filterBySearch = companies.filter((item) => {
+      item.filter((c) => {
+        
+        if(c.name === searchVal){
+          return c;
+        }
+        return companies;
+      })
+      return companies;
+    })
+    setCompanies(filterBySearch);
+}
 
-      <Grid container columnSpacing={35} rowSpacing={5} >
-        {companies.map((company) => (
-          <Grid item md={4}>
-            <Card name={company.name} title={company.title} jobDescription={company.jobDescription} expReq={company.expReq} minSal={company.minSal} location={company.location}/>
-          </Grid>
-        ))}
-      </Grid>
-      
-    </div>
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Box>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={e => setSearchVal(e.target.value)}/>
+          <BsSearch onClick={handleSearchClick} />
+        </Box>
+        {company.map((comp) => {
+          return (
+            <Box sx={{ height: "100vh", width: "100vw" }}>
+          <AutoSizer>
+            {({ height, width }) => (
+              <Grid 
+                cellRenderer={({ columnIndex, key, rowIndex, style }) => (
+                  <JobCard
+                    key={key}
+                    data={company[rowIndex][columnIndex]}
+                    style={style}
+                  />
+                )}
+                columnCount={company[0].length}
+                columnWidth={() =>
+                  calculateColumnWidth(company[0].length, width)
+                }
+                height={height}
+                rowCount={companies.length}
+                rowHeight={600}
+                width={width}
+              />
+            )}
+          </AutoSizer>
+        </Box>
+          )
+        })}
+        
+      </Fragment>
+    </ThemeProvider>
   );
 }
 
--- src/jobCard/JobCard.js
+++ src/jobCard/JobCard.js
@@ -1,78 +1,93 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Button from '@mui/material/Button';
-import BoltIcon from '@mui/icons-material/Bolt';
-import img from '../images/test.jpg';
-import ShowMoreText from "react-show-more-text";
-import './JobCard.css';
+import { useState, useCallback } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import BoltIcon from "@mui/icons-material/Bolt";
 
+// Images
+import img from "../images/test.jpg";
 
-const theme = createTheme({
-  palette: {
-    neutral: {
-      main: "#94FFD8",
-      contrastText: "#black"
-    },
-    danger: {
-      main: "#10439F",
-      contrastText: "#fff"
-    },
-    icon: {
-      main: "#F3CA52"
-    }
-  }
-});
+const toggleShowwMore = (setShowMore) => !setShowMore;
 
-export default function RecipeReviewCard(props) {
+export default function JobCard({
+  data: {
+    name,
+    title,
+    expReq,
+    minSal,
+    maxSal,
+    location,
+    jobDescription = "",
+  } = {},
+  height,
+  style,
+}) {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleShowMore = useCallback(() => setShowMore(toggleShowwMore), []);
+
   return (
-    <Card sx={{ maxWidth: 345 }} className= "Card">
-      <CardMedia
-        component="img"
-        height="194"
-        image={img}
-        alt={props.name}
-      />
-      <CardHeader
-        title={props.name}
-        subheader={props.title}
-      />
-      <CardContent>
-        Experience Required: {props.expReq} years
-        <br/> 
-        Expected Salary: {props.minSal} - {props.maxSal} LPA
-        <br/> 
-        Location: {props.location}
-      </CardContent>
-      <CardContent className="content">
-        <h3>
-          About Company:
-        </h3>
-        <ShowMoreText lines={3} more="Show more"
-                less="Show less"
-                className="content-css"
-                anchorClass="show-more-less-clickable"
-                expanded={false}
-                truncatedEndingComponent={"... "}>
-          {props.jobDescription}
-        </ShowMoreText>
-        
-      </CardContent>
-      <ThemeProvider theme={theme}>
-      <div className="Buttons">
-        <div className="Easy_Apply_Div">
-          <Button variant="contained" fullWidth = {true} color="neutral" disableElevation={true} startIcon={<BoltIcon color="icon"/>}>Easy Apply</Button>
-        </div>
-        <div>
-          <Button variant="contained" fullWidth = {true} color="danger" disableElevation={true}>Ask for referral</Button>
-        </div>
-      </div>
-      </ThemeProvider>
-      
-      
-    </Card>
+    <Box sx={{ padding: 2, height }}>
+      <Card
+        sx={{ height: 1, display: "flex", flexDirection: "column", marginBottom: "100px"}}
+        style={style}
+      >
+        <CardMedia component="img" height="150" image={img} alt={name} />
+
+        <CardHeader title={name} subheader={title} />
+
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Box sx={{ height: 200, overflowY: "scroll" }}>
+            <Box>Experience Required: {expReq} years</Box>
+            <Box>
+              Expected Salary: {minSal} - {maxSal} LPA
+            </Box>
+            <Box>Location: {location}</Box>
+
+            <Box>
+              <h3>About Company:</h3>
+              <Box>
+                {showMore
+                  ? jobDescription
+                  : `${jobDescription.substring(0, 30)}...`}
+
+                {!showMore && (
+                  <Button
+                    onClick={handleShowMore}
+                    variant="text"
+                    disableElevation={true}
+                  >
+                    Show More
+                  </Button>
+                )}
+              </Box>
+            </Box>
+          </Box>
+
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 1 }}>
+            <Button
+              variant="contained"
+              fullWidth={true}
+              color="neutral"
+              disableElevation={true}
+              startIcon={<BoltIcon color="icon" />}
+            >
+              Easy Apply
+            </Button>
+            <Button
+              variant="contained"
+              fullWidth={true}
+              color="danger"
+              disableElevation={true}
+            >
+              Ask for referral
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
